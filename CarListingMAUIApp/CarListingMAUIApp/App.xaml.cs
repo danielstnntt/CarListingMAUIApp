f9f6644e46d9c9a9b4d4f207b4af993f793e95f4
Daniel Stennett <Daniel.Stennett@farsound.com>
2025-06-17 10:38:35 +0100

    Initial Commit

---
+++ CarListingMAUIApp/CarListingMAUIApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CarListingMAUIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
